c2cd4fe3a5027180a427375c78024a4dfdecf957
jesus-rs-0803 <jesus.enggel.rivera.santiago@everis.com>
2020-07-16 21:05:59 -0500

    commit proyecto

---
+++ src/main/java/com/bootcam/cuentasBancarias/MicroserviceCuentasBancariasApplication.java
@@ -0,0 +1,13 @@
+package com.bootcam.cuentasBancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceCuentasBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceCuentasBancariasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bootcam/cuentasBancarias/MicroserviceCuentasBancariasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcam.cuentasBancarias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceCuentasBancariasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
